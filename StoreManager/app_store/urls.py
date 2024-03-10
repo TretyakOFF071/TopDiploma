7abd75379994d68aac674f3a14cba86a5bf41cf5
vadim <tretyakoff071@gmail.com>
2024-03-10 16:10:45 +0500

    fix

--- StoreManager/app_store/urls.py
+++ StoreManager/app_store/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls.static import static
 from django.urls import path, include
 from django.conf import settings
 
-from .views import register_view, CustomLoginView, CustomLogoutView, ProfileView,ProviderListView, \
-    GoodCategoryListView,delete_provider, edit_provider,GoodsListView, delete_good, edit_good, delete_category, \
-    GoodDetailView, create_supply, SupplyListView, CreateSaleView,SalesListView, GetGoodPriceView
+from .views import register_view, CustomLoginView, CustomLogoutView, ProfileView, ProviderListView, \
+    GoodCategoryListView, delete_provider, edit_provider, GoodsListView, delete_good, edit_good, delete_category, \
+    GoodDetailView, create_supply, SupplyListView, CreateSaleView, SalesListView, GetGoodPriceView
 from .api_views import ProviderAPIView, GoodAPIView, ProviderDetailAPI, GoodDetailAPI
 
 urlpatterns = [
